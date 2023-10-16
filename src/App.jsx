ee22bb0ff0847ad7708a775b60960caacfa8c3bb
mubariznacafov <mubariz.necefov.17@gmail.com>
2023-10-16 18:37:25 +0400

    some changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,10 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
-import Home from "./components/home";
-import About from "./components/about";
-import Menu from "./components/menu";
-import Contact from "./components/contact";
+import Home from "./components/Home";
+import About from "./components/About";
+import Menu from "./components/Menu";
+import Contact from "./components/Contact";
 
 const App = () => {
   return (
